3ae2400ad4e4bfa401f6e4cfecfad648184f9e41
anohene1 <anoheneisaac@gmail.com>
2022-03-25 08:19:47 +0000

    set up environmental variables

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBlXJO-mVHPAhWMWHyd6dOL0x_xYCFUOkU',
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'igclone-3226a.firebaseapp.com',
   projectId: 'igclone-3226a',
   storageBucket: 'igclone-3226a.appspot.com',
-  messagingSenderId: '712628385145',
-  appId: '1:712628385145:web:3866034df8074b8e78eba8',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export { app, db, storage }
