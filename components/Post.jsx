968d35a5c8aa3c454e369e3794706115bc36e2ee
anohene1 <anoheneisaac@gmail.com>
2022-03-29 11:40:28 +0000

    implement post like

--- components/Post.jsx
+++ components/Post.jsx
@@ -7,7 +7,7 @@ import {
   PaperAirplaneIcon,
 } from '@heroicons/react/outline'
 
-import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+import { HeartIcon as FilledHeartIcon } from '@heroicons/react/solid'
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import {
@@ -17,6 +17,9 @@ import {
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
+  doc,
+  deleteDoc,
 } from 'firebase/firestore'
 import { db } from '../firebase'
 import Moment from 'react-moment'
@@ -25,6 +28,8 @@ function Post({ id, caption, img, userImg, username }) {
   const { data: session } = useSession()
   const [comments, setComments] = useState([])
   const [comment, setComment] = useState('')
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
 
   async function sendComment(event) {
     event.preventDefault()
@@ -40,6 +45,19 @@ function Post({ id, caption, img, userImg, username }) {
     })
   }
 
+  async function likePost(event) {
+    event.preventDefault()
+
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid))
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      })
+    }
+  }
+
+  // Fetch post comments
   useEffect(
     function () {
       return onSnapshot(
@@ -52,7 +70,30 @@ function Post({ id, caption, img, userImg, username }) {
         }
       )
     },
-    [db]
+    [db, id]
+  )
+
+  // Fetch post likes
+  useEffect(
+    function () {
+      return onSnapshot(
+        query(collection(db, 'posts', id, 'likes')),
+        (snapshot) => {
+          setLikes(snapshot.docs)
+        }
+      )
+    },
+    [db, id]
+  )
+
+  // Check if user has liked post
+  useEffect(
+    function () {
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      )
+    },
+    [likes]
   )
 
   return (
@@ -72,7 +113,14 @@ function Post({ id, caption, img, userImg, username }) {
       {session && (
         <div className="flex justify-between px-4 pt-4">
           <div className="flex space-x-4">
-            <HeartIcon className="btn" />
+            {hasLiked ? (
+              <FilledHeartIcon
+                onClick={likePost}
+                className="btn text-red-500"
+              />
+            ) : (
+              <HeartIcon onClick={likePost} className="btn" />
+            )}
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn rotate-45" />
           </div>
@@ -82,6 +130,11 @@ function Post({ id, caption, img, userImg, username }) {
       )}
 
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">
+            {likes.length} {likes.length === 1 ? 'like' : 'likes'}
+          </p>
+        )}
         <span className="font-bold mr-1">{username} </span>
         {caption}
       </p>
